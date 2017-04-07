9281cc4d72c0fe96198c3b8b37f8c5285d95bd1d
Ilene Gorski <ilene.gorski@gmail.com>
2017-04-07 11:53:25 -0700

    Include number and text in input options, add different functions to determine which value to use

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,41 @@ function Pizza(size, toppings) {
 }//close constructor
 //price prototype
 Pizza.prototype.price = function() {
-  price = 10;
+  price = 0;
   this.toppings.forEach(function(topping) {
     price += topping;
   })
-  price*=(this.size/3)
+  price*=(this.size/2);
+  price += 10;
   price*=1.07;
   console.log(price);
 }
 //end prototype
 toppings = [];
+toppingsList = [];
 //UI
 $(function () {
   $("form").submit(function(event) {
     event.preventDefault();
+    // determine size and toppings from user input
+    var sizeText = $("#size").val().split(",")[1];
     var size = parseFloat($("#size").val());
-    var sizeText = $("#size").val();
-    console.log(sizeText);
+    $("input:checkbox[name='toppings']:checked").each(function(){
+      toppingsList.push($(this).val().split(",")[1]);
+    });
     $("input:checkbox[name='toppings']:checked").each(function(){
       toppings.push(parseFloat($(this).val()));
     });
+
+    // create Pizza instance
     var newPizza = new Pizza(size, toppings)
+    // run price prototype
     newPizza.price();
-    $("#sizeOutput").text('BIG');
-    $("#toppings").text("YUMMY");
-    $("#price").text("EXPENSIVE");
+    console.log(newPizza);
+    // update receipt
+    $("#sizeOutput").text(sizeText);
+    $("#toppings").text(toppingsList);
+    $("#price").text(price.toFixed(2));
     $("form").hide();
     $("#receipt").show();
   });
