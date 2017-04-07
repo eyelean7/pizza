1e981fb4b430616d633fb142528a986bea9b6ed3
Ilene Gorski <ilene.gorski@gmail.com>
2017-04-07 14:14:38 -0700

    Make better output option when no toppings are selected

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 |Accept user input - size|
 |Accept user input - toppings|
 |Update Pizza instance with user input|
-|Generate price from pizza qualities|
+|Generate price from pizza qualities| size: small, toppings: pepperoni, mushroom| $13.91|
--- js/scripts.js
+++ js/scripts.js
@@ -20,16 +20,31 @@ Pizza.prototype.price = function() {
   console.log(total);
 }
 //end prototype
+//verify toppings
+function valToppings() {
+  var checkboxs=document.getElementsByName("toppings");
+  var okay=false;
+  for(var i=0,l=checkboxs.length;i<l;i++) {
+    if(checkboxs[i].checked) {
+      okay=true;
+      break;
+    }
+  }
+  if(okay) {
+    toppingsList.pop();
+  };
+}//end verify toppings
 var toppings = [];
-var toppingsList = [];
+var toppingsList = ["no toppings"];
 var sizeText = "";
 var size = 1;
 function addPizza() {
   toppings = [];
-  toppingsList = [];
+  toppingsList = ["no toppings"];
   // determine size and toppings from user input
   sizeText = $("#size").val().split(",")[1];
   size = parseFloat($("#size").val());
+  valToppings();
   $("input:checkbox[name='toppings']:checked").each(function(){
     toppingsList.push($(this).val().split(",")[1]);
   });
@@ -54,11 +69,16 @@ $(function () {
   $("#myForm").submit(function(event) {
     event.preventDefault();
     // update receipt
-    $("#price").text(total.toFixed(2));
-    $("form").hide();
-    $("#receipt").show();
-    if (numberOfPizzas > 1) {
-      $("#plural").text("s");
+    if (numberOfPizzas < 1) {
+      alert("Please add at least one pizza to your order.")
+    }
+    else {
+      $("#price").text(total.toFixed(2));
+      $("form").hide();
+      $("#receipt").show();
+      if (numberOfPizzas > 1) {
+        $("#plural").text("s");
+      }
     }
   });
 });
